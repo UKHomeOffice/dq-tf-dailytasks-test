5e82528b38b5ea969af1d6c372bf52ffe5f3546b
Jwanderema <wanderema@hotmail.co.uk>
2020-03-24 11:54:45 +0000

    refactor cloudwatch

--- rds_shutdown.tf
+++ rds_shutdown.tf
@@ -16,7 +16,7 @@ resource "aws_lambda_function" "rds_shutdown_function" {
   role             = "${aws_iam_role.rds_shutdown_role.arn}"
   filename         = "${data.archive_file.rds_shutdownzip.output_path}"
   memory_size      = 128
-  timeout          = "10"
+  timeout          = "900"
   source_code_hash = "${data.archive_file.rds_shutdownzip.output_base64sha256}"
 
   tags = {
@@ -51,70 +51,130 @@ EOF
 
 ### IAM Policy Documents
 
-data "aws_iam_policy_document" "eventwatch_logs_doc" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:DescribeLogStreams",
-      "logs:GetLogEvents"
-    ]
-    resources = [
-      "arn:aws:logs:*:*:*",
-    ]
-  }
-}
+# data "aws_iam_policy_document" "eventwatch_logs_doc" {
+#   statement {
+#     actions = [
+#       "logs:CreateLogGroup",
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents",
+#       "logs:DescribeLogStreams",
+#       "logs:GetLogEvents"
+#     ]
+#     resources = [
+#       "arn:aws:logs:*:*:*",
+#     ]
+#   }
+# }
 
-data "aws_iam_policy_document" "eventwatch_rds_doc" {
-  statement {
-    actions = [
-      "rds:DescribeDBInstances",
-      "rds:StartDBInstances",
-      "rds:StopDBInstances",
-      "rds:CopyDBSnapshot",
-      "rds:CreateDBSnapshot",
-      "rds:DeleteDBSnapshot"
-    ]
-    resources = [
-      "*"
-    ]
-  }
-}
+# data "aws_iam_policy_document" "eventwatch_rds_doc" {
+#   statement {
+#     actions = [
+#       "rds:DescribeDBInstances",
+#       "rds:StartDBInstances",
+#       "rds:StopDBInstances",
+#       "rds:CopyDBSnapshot",
+#       "rds:CreateDBSnapshot",
+#       "rds:DeleteDBSnapshot"
+#     ]
+#     resources = [
+#       "*"
+#     ]
+#   }
+# }
 
 ### IAM Policies
 
-resource "aws_iam_policy" "eventwatch_logs_policy" {
-  name   = "eventwatch-logs-policy"
-  path   = "/"
-  policy = "${data.aws_iam_policy_document.eventwatch_logs_doc.json}"
+resource "aws_iam_policy" "rds_shutdown" {
+  name        = "${var.pipeline_name}-rds-shutdown"
+  path        = "/"
+  description = "IAM policy for shutting down rds"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Action": [
+              "rds:DescribeDBInstances",
+              "rds:StartDBInstances",
+              "rds:StopDBInstances",
+              "rds:CopyDBSnapshot",
+              "rds:CreateDBSnapshot",
+              "rds:DeleteDBSnapshot"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
 
-resource "aws_iam_policy" "eventwatch_rds_policy" {
-  name   = "eventwatch-rds-policy"
-  path   = "/"
-  policy = "${data.aws_iam_policy_document.eventwatch_rds_doc.json}"
-}
+# resource "aws_iam_policy" "eventwatch_rds_policy" {
+#   name   = "eventwatch-rds-policy"
+#   path   = "/"
+#   policy = "${data.aws_iam_policy_document.eventwatch_rds_doc.json}"
+# }
 
 ### IAM Policy Attachments
 
-resource "aws_iam_role_policy_attachment" "eventwatch_rds_shutdown_logs_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "rds_shutdown" {
   role       = "${aws_iam_role.rds_shutdown_role.name}"
-  policy_arn = "${aws_iam_policy.eventwatch_logs_policy.arn}"
+  policy_arn = "${aws_iam_policy.rds_shutdown.arn}"
 }
 
-resource "aws_iam_role_policy_attachment" "eventwatch_rds_shutdown_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "lambda_rds_shutdown_logging" {
   role       = "${aws_iam_role.rds_shutdown_role.name}"
-  policy_arn = "${aws_iam_policy.eventwatch_rds_policy.arn}"
+  policy_arn = "${aws_iam_policy.lambda_rds_shutdown_logging.arn}"
+}
+
+# Creates CloudWatch Log Group
+
+resource "aws_cloudwatch_log_group" "lambda_rds_shutdown" {
+  name              = "/aws/lambda/${aws_lambda_function.rds_shutdown_function.function_name}"
+  retention_in_days = 14
+
+  tags = {
+    Name = "rds-shutdown-${local.naming_suffix}"
+  }
+}
+
+resource "aws_iam_policy" "lambda_rds_shutdown_logging" {
+  name        = "${var.pipeline_name}-rds_shutdown-logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams",
+        "logs:GetLogEvents"
+      ],
+      "Resource": [
+        "${aws_cloudwatch_log_group.lambda_rds_shutdown.arn}",
+        "${aws_cloudwatch_log_group.lambda_rds_shutdown.arn}/*"
+      ],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
 }
 
 # Creates CloudWatch Event Rule - triggers the Lambda function
 
 resource "aws_cloudwatch_event_rule" "daily_rds_shutdown" {
-  is_enabled          = "${var.naming_suffix == "apps-prod-dq" ? "false" : "false"}"
+  is_enabled          = "true"
   name                = "daily-rds-shutdown"
   description         = "triggers daily RDS shutdown"
-  schedule_expression = "cron(15 11 * * ? *)"
+  schedule_expression = "cron(10 12 * * ? *)"
 }
 
 # Defines target for the rule - the Lambda function to trigger
