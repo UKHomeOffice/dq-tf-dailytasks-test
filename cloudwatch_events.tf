244f3719e810f2b3ca7c27a2a02efd9506e17512
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2021-02-24 14:25:14 +0000

    first drone run

--- cloudwatch_events.tf
+++ cloudwatch_events.tf
@@ -1,3 +1,26 @@
+resource "aws_cloudwatch_event_target" "rds_shutdown" {
+  rule = aws_cloudwatch_event_rule.rds_shutdown.name
+  arn  = aws_lambda_function.rds_shutdown.arn
+
+  input = <<DOC
+{
+  "instances": [
+    "fms-postgres-fms-apps-test-dq",
+    "postgres-datafeeds-apps-test-dq",
+    "postgres-internal-tableau-apps-test-dq"
+  ],
+  "action": "stop"
+}
+DOC
+}
+
+resource "aws_cloudwatch_event_rule" "rds_shutdown" {
+  name                = "rds-shutdown"
+  description         = "Shutdown RDS Instances"
+  schedule_expression = "cron(05 15 * * ? *)"
+  is_enabled          = "false"
+}
+
 resource "aws_cloudwatch_event_target" "ec2_shutdown" {
   rule = aws_cloudwatch_event_rule.ec2_shutdown.name
   arn  = aws_lambda_function.ec2_shutdown.arn
